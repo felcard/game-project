245865abecfe0516a0c8cf0aeae3054f60587328
felcard <zundroid@gmail.com>
2022-11-16 14:25:44 +0000

    complete 5-get-reviews-by-id

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { getCategories, getReviews } = require('./controllers/controllers.js');
-const { catchAll } = require('./controllers/errors');
+const { getCategories, getReviews, getReviewById } = require('./controllers/controllers.js');
+const { catchAll, customErrors, psqlErrors } = require('./controllers/errors');
 
 const app = express();
 
 app.get('/api/categories', getCategories);
 app.get('/api/reviews', getReviews);
+app.get('/api/reviews/:review_id', getReviewById);
+
+app.use(psqlErrors);
+app.use(customErrors);
+
 
 app.all('/*', (req, res) => {
     res.status(404).send({ msg: "Wrong URL!" });
