f59584cb6457fde8f5af4077a79444bdc9d11a8b
felcard <zundroid@gmail.com>
2022-11-16 11:08:05 +0000

    complete 4-get-api-reviews

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const db = require('../db/connection.js');
 const seed = require('../db/seeds/seed.js');
 const seedingData = require('../db/data/test-data/index.js');
+const { } = require('../db/seeds/utils.js');
 
 beforeEach(() => seed(seedingData));
 afterAll(() => db.end());
@@ -22,6 +23,40 @@ describe('api/categories', () => {
                     });
                 });
                 expect();
+            });
+    });
+    test('GET:404 responds with a message of "Wrong URL" when provided with wrong end point', () => {
+        return request(app)
+            .get('/api/badURL')
+            .expect(404)
+            .then((response) => {
+                expect(response.body.msg).toBe('Wrong URL!');
+            });
+    });
+});
+
+describe.only('/api/reviews', () => {
+    test('GET:200 responds with array of reviews with properties: owner(which is the `username` from the users table), title, review_id, category, review_img_url, created_at, votes, designer, comment_count which is the total count of all the comments with this review_id. the reviews should be sorted by date in descending order', () => {
+        return request(app)
+            .get('/api/reviews')
+            .expect(200)
+            .then(response => {
+                const reviews = response.body.reviews;
+                expect(reviews.length === 13).toBe(true);
+                expect(reviews[0]).toEqual(expect.objectContaining({
+                    owner: 'mallionaire',
+                    title: 'Mollit elit qui incididunt veniam occaecat cupidatat',
+                    review_id: 7,
+                    category: 'social deduction',
+                    review_img_url: 'https://images.pexels.com/photos/278888/pexels-photo-278888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+                    created_at: '2021-01-25T11:16:54.963Z',
+                    votes: 9,
+                    designer: 'Avery Wunzboogerz',
+                    comment_count: '0'
+                }));
+                expect(reviews).toBeSortedBy('created_at', {
+                    descending: true,
+                });
             });
     });
     test('GET:404 responds with a message of "Wrong URL" when provided with wrong end point', () => {
