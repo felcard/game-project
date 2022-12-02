950b0af3377b7447459fc8c84b641d605c6fbed1
felcard <zundroid@gmail.com>
2022-12-02 01:01:30 +0000

    remove console log in listen file

--- listen.js
+++ listen.js
@@ -3,9 +3,5 @@ const app = require("app");
 const { PORT = 7777 } = process.env;
 
 app.listen(PORT, () => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`listen on port ${PORT}`);
-  }
+  console.log(`listen on port ${PORT}`);
 });
